4c50e7d6c8ae70b7f787365475ba18e5fda53501
Diponkor99 <roydiponker891@gmail.com>
2024-09-24 22:55:07 +0600

    History done

---
+++ js/AddMoney.js
@@ -0,0 +1,80 @@
+// card-1 start------1
+document.getElementById('Donate-btn1').addEventListener('click',function(){
+  
+   const add_con= AddInputFile('input-filed1');
+   const Sub_card=SubtractionCard('Main_maney');
+   if(isNaN(add_con)){
+    alert('Please try again');
+      return;
+   }
+
+   if(Sub_card < add_con){
+      alert('NOt Enought money');
+        return;
+     }
+   const box1=AddCard('coya-Add1');
+   const Add_Money=box1+add_con;
+   document.getElementById('coya-Add1').innerText=Add_Money;
+   
+   const Sub_money=Sub_card - add_con;
+   document.getElementById('Main_maney').innerText=Sub_money;
+   const noakhali =document.getElementById('Noakhali').innerText;
+   History(add_con, noakhali);
+ 
+   
+ }
+ )
+// card-1 End------1
+
+
+// card-2----start------2-->
+document.getElementById('Btn_2').addEventListener('click',function(){
+   const add_con= AddInputFile('input_text_2');
+   const Sub_card=SubtractionCard('Main_maney');
+   if(isNaN(add_con)){
+    alert('Please try again');
+      return;
+   }
+   if(Sub_card < add_con){
+      alert('NOt Enought money');
+        return;
+     }
+   const box1=AddCard('Amount_box-2');
+   const Add_Money=box1+add_con;
+   document.getElementById('Amount_box-2').innerText=Add_Money;
+    const Sub_money=Sub_card - add_con;
+   document.getElementById('Main_maney').innerText=Sub_money;
+   const name2=document.getElementById('Feni').innerText;
+   History(add_con,name2);
+ }
+ )
+// card-2----End------2
+
+
+// card-3----start------3--->
+document.getElementById('btn_3').addEventListener('click',function(){
+   const add_con1= AddInputFile('input_text_3');
+   const Sub_card=SubtractionCard('Main_maney');
+   if(isNaN(add_con1)){
+    alert('Please try again');
+      return;
+   }
+
+   if(Sub_card < add_con1){
+      alert('NOt Enought money');
+        return;
+     }
+   const box1=AddCard('Box_card_3');
+   const Add_Money3=box1 + add_con1;
+   document.getElementById('Box_card_3').innerText=Add_Money3;
+   const Sub_money=Sub_card - add_con1;
+   document.getElementById('Main_maney').innerText=Sub_money;
+
+   const quota=document.getElementById('Quota').innerText;
+   History(add_con1,quota);
+ 
+   
+ }
+ )
+// card-3----End------3
+
--- js/function.js
+++ js/function.js
@@ -3,14 +3,62 @@ function Button(id){
     document.getElementById('history').classList.add('hidden')
     document.getElementById(id).classList.remove('hidden');
 
-    
-    
-    
-
 }
+
+
+// Button---color---Start--->   
 document.getElementById("dp").style.backgroundColor = "transparent";
 function bgColor(id){
     document.getElementById("Do").style.backgroundColor = "transparent";;
     document.getElementById("dp").style.backgroundColor = "transparent";;
     document.getElementById(id).style.backgroundColor='';
 }
+// Button---color---End
+
+// Input---money---From--start--->
+function AddInputFile(element){
+    const input =document.getElementById(element).value;
+    const input_float=parseFloat(input);
+    return input_float;
+}
+// Input---money---From--End
+
+
+// Add-----Money---card---Start--->
+function AddCard(id){
+    const Add_money=document.getElementById(id).innerText;
+    const float_AddMoney=parseFloat(Add_money);
+    return float_AddMoney;
+}
+// Add-----Money---card---End
+
+// Subtraction-----Money---card--start----->
+function SubtractionCard(id){
+    const Sub=document.getElementById(id).innerText;
+    const Float_Sub=parseFloat(Sub);
+    return Float_Sub;
+}
+// Subtraction-----Money---card--End
+
+
+// History---function--start
+function History(Tk, NAME){
+  const div= document.createElement('div');
+  div.style.boxShadow='2xl';
+  div.classList.add('bg-stone-300')
+  div.style.padding='20px'
+  div.style.marginBottom='20px'
+  div.style.border="2px solid black"
+  div.style.borderRadius='10px'
+  const date=new Date();
+  div.innerHTML=`
+  <h1> ${Tk} Taka is ${NAME} </h1>
+  <br>
+  <p>Date:${date}</P>
+  `
+  
+        
+document.getElementById('history').appendChild(div);
+
+}
+// History---function--End
