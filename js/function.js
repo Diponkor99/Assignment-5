87f9bfcec97b0be0af6261064e8936723ce87776
Diponkor99 <roydiponker891@gmail.com>
2024-09-24 13:49:42 +0600

    first commit

---
+++ js/function.js
@@ -0,0 +1,16 @@
+function Button(id){
+    document.getElementById('main').classList.add('hidden');
+    document.getElementById('history').classList.add('hidden')
+    document.getElementById(id).classList.remove('hidden');
+
+    
+    
+    
+
+}
+document.getElementById("dp").style.backgroundColor = "transparent";
+function bgColor(id){
+    document.getElementById("Do").style.backgroundColor = "transparent";;
+    document.getElementById("dp").style.backgroundColor = "transparent";;
+    document.getElementById(id).style.backgroundColor='';
+}
---
+++ js/style.js
@@ -0,0 +1,11 @@
+document.getElementById('Donation').addEventListener('click',function(){
+    Button("main");
+    bgColor('Do')
+   
+})
+
+document.getElementById('History').addEventListener('click',function(){
+    Button('history');
+    bgColor('dp');
+})
+
